57cfd7a477c9ecc5b3583c71162197bb0c8c536f
AlgoKnight11 <bhavyapandey22995@gmail.com>
2026-07-30 12:50:28 +0530

    Optimized it further

--- arrayManipulation.py
+++ arrayManipulation.py
@@ -1,8 +1,14 @@
 def arrayManipulation(n, queries):
     # Write your code here
-    results=[0]*n
-    for i in queries:
-        for j in range(i[0]-1,i[1]):
-            results[j]=results[j]+i[2]
-    return max(results)
+    results=[0]*(n+2)
+    for a,b,k in queries:
+        results[a]+=k
+        results[b+1]-=k
+    max_value=0
+    current_sum=0
+    for i in range(0,n+1):
+        current_sum+=results[i]
+        if current_sum>max_value:
+            max_value=current_sum
+    return max_value
 print(arrayManipulation(10,[[1,5,3],[4,8,7],[6,9,1]]))
